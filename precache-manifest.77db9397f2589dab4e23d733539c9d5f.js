8932fe58e7ae9647036b1cd111df7867fe121947
Niccolò Diana <niko047@hotmail.it>
2019-12-14 11:23:00 +0900

    Updates

---
+++ precache-manifest.77db9397f2589dab4e23d733539c9d5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92e1f883ec3703cbb19ee12664b99139",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "87617a68c594f22771f8",
+    "url": "/react-gh-pages/static/css/main.c49690dc.chunk.css"
+  },
+  {
+    "revision": "98cc8dfa516f41e2d20a",
+    "url": "/react-gh-pages/static/js/2.9a1a1bb2.chunk.js"
+  },
+  {
+    "revision": "87617a68c594f22771f8",
+    "url": "/react-gh-pages/static/js/main.f9b1c04a.chunk.js"
+  },
+  {
+    "revision": "61840952648088723da4",
+    "url": "/react-gh-pages/static/js/runtime-main.46f1e905.js"
+  }
+]);
